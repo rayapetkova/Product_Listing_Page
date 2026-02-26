f0e724a6bdbaeaf8e0aa9f0225d10c74562dada8
Raya Petkova <rayapetkova2007@abv.bg>
2026-02-26 22:14:50 +0200

    Update SortBar component and move it to new folder

---
+++ client/src/components/sortBar/SortBar.jsx
@@ -0,0 +1,27 @@
+import styles from './SortBar.module.css';
+
+const SortBar = ({ productsCount, sortingOptionSetter }) => {
+    const sortingOptionChangeHandler = (e) => {
+        sortingOptionSetter(e.target.value);
+    }
+
+    return (
+        <div className={styles["sort-bar"]}>
+            <div className={styles["product-counter"]}>
+                3 out of {productsCount} products displayed
+            </div>
+            
+            <div className={styles["sort-dropdown"]}>
+                <label htmlFor="sort">Sort by: </label>
+                <select id="sort" onChange={sortingOptionChangeHandler}>
+                    <option value="az">Alphabetical (A–Z)</option>
+                    <option value="za">Alphabetical (Z–A)</option>
+                    <option value="lowHigh">Price (Low to High)</option>
+                    <option value="highLow">Price (High to Low)</option>
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default SortBar;
