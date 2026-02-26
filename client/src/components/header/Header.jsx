8e91de47407e957ed037d66897cd038751166a8c
Raya Petkova <rayapetkova2007@abv.bg>
2026-02-26 22:11:06 +0200

    Update Header component with new links

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={styles["header"]}>
+            <div className={styles["header-content"]}>
+                <Link to="/"><h2>RP Store</h2></Link>
+                <nav>
+                    <Link to="/categories/bags">Bags</Link>
+                    <Link to="/categories/shoes">Shoes</Link>
+                    <Link to="/cart">Cart</Link>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
