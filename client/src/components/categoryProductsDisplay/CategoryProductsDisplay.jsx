edcd632a0e3862a29d73a3b1b62c98fd0601a710
Raya Petkova <rayapetkova2007@abv.bg>
2026-02-27 03:58:08 +0200

    Load more functionality for the category products display component

--- client/src/components/categoryProductsDisplay/CategoryProductsDisplay.jsx
+++ client/src/components/categoryProductsDisplay/CategoryProductsDisplay.jsx
@@ -10,12 +10,15 @@ import { useParams } from 'react-router-dom';
 const CategoryProductsDisplay = () => {
     const [categoryProducts, setCategoryProducts] = useState([])
     const [sortingOption, setSortingOption] = useState("az");
+    const [visibleProductsCount, setVisibleProductsCount] = useState(5);
     const { categoryName } = useParams();
 
     useEffect(() => {
         function loadProducts() {
-            const products = getProducts({categories: [categoryName]}, sortingOption);
+            const products = getProducts({ categories: [categoryName] }, sortingOption);
             setCategoryProducts(products);
+
+            setVisibleProductsCount(products.length < 5 ? products.length : 5);
         }
 
         loadProducts();
@@ -28,20 +31,21 @@ const CategoryProductsDisplay = () => {
     return (
         <>
             <h1>{categoryName}</h1>
-            
+
             <div className={styles["content-layout"]}>
                 <section className={styles["products-section"]}>
 
-                    <SortBar productsCount={categoryProducts.length} sortingOptionSetter={sortingOptionSetter} />
+                    <SortBar visibleProductsCount={visibleProductsCount} productsCount={categoryProducts.length} sortingOptionSetter={sortingOptionSetter} />
 
-                    <ProductGrid products={categoryProducts} sortingOption={sortingOption} />
+                    <ProductGrid products={categoryProducts.slice(0, visibleProductsCount)} sortingOption={sortingOption} />
 
-                    <div className={styles["load-more-container"]}>
-                        <button className={styles["load-more-btn"]}>
-                            Load More
-                        </button>
-                    </div>
-
+                    {visibleProductsCount < categoryProducts.length && (
+                        <div onClick={loadMoreProductsHandler} className={styles["load-more-container"]}>
+                            <button className={styles["load-more-btn"]}>
+                                Load More
+                            </button>
+                        </div>
+                    )}
                 </section>
             </div>
         </>
